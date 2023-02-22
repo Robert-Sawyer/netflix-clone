cbde8645c076062c686ee968ff44526959b542f9
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2023-02-22 15:39:11 +0100

    Added global font and some banner styling

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import HomeScreen from "./components/HomeScreen/HomeScreen";
+import WebFont from 'webfontloader';
 
 function App() {
+
+    useEffect(() => {
+        WebFont.load({
+            google: {
+                families: ['Helvetica Neue']
+            }
+        });
+    }, []);
+
     return (
         <div className="App">
             <HomeScreen/>
--- src/components/HomeScreen/Banner.js
+++ src/components/HomeScreen/Banner.js
@@ -2,6 +2,11 @@ import React from "react";
 import './Banner.css';
 
 const Banner = () => {
+
+    const truncateText = (text, number) => {
+        return text?.length > number ? text.substr(0, number - 1) + '...' : text;
+    }
+
     return (
         <header className='banner' style={{
             backgroundPosition: 'center center',
@@ -14,7 +19,12 @@ const Banner = () => {
                     <button className='banner__button'>Play</button>
                     <button className='banner__button'>Add to list</button>
                 </div>
-                <h1 className='banner__description'>Test description</h1>
+                <h1 className='banner__description'>{truncateText(`Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea 
+                commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+                fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                deserunt mollit anim id est laborum.`, 150)}</h1>
             </div>
             <div className='banner--fadeButton'/>
         </header>
