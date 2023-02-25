5df2b72b2d9454b5a4b32cb38b2b8ed778dfb135
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2023-02-25 21:45:24 +0100

    Little fix

--- src/components/HomeScreen/Row.js
+++ src/components/HomeScreen/Row.js
@@ -21,14 +21,17 @@ const Row = ({title, fetchUrl, isLargeRow}) => {
         <div className='row'>
             <h2>{title}</h2>
             <div className='row_posters'>
-                {movies?.map(movie => (
+                {movies?.map(movie =>
+                    ((isLargeRow && movie.poster_path) ||
+                    (!isLargeRow && movie.backdrop_path)) && (
                     <img
                         key={movie.id}
                         className={`row_poster ${isLargeRow && 'row_posterLarge'}`}
                         src={`${baseImageUrl}${
                             isLargeRow ? movie.poster_path : movie.backdrop_path
                         }`} alt={movie.name}/>
-                ))}
+                )
+                )}
             </div>
 
         </div>
