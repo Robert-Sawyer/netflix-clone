fa75b483dbc6fe285bfebc9bcfe82813371c5763
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2023-03-01 11:31:39 +0100

    Improved Login screen with styling

--- src/components/HomeScreen/Nav.js
+++ src/components/HomeScreen/Nav.js
@@ -24,7 +24,9 @@ const Nav = () => {
 
             </div>
             <img className='nav__logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt=''/>
-            <img className='nav__avatar-icon' src='https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-squares-01/3/30-512.png' alt=''/>
+            <a href='/login'>
+                <img className='nav__avatar-icon' src='https://cdn4.iconfinder.com/data/icons/social-messaging-ui-color-squares-01/3/30-512.png' alt=''/>
+            </a>
         </div>
     )
 }
--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -1,7 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 import './LoginScreen.css';
+import SignUpScreen from "../SignIn/SignUpScreen";
 
 const LoginScreen = () => {
+    const [signIn, setSignIn] = useState(false);
+
     return (
         <div className='loginScreen'>
             <div className='loginScreen__background'>
@@ -10,14 +13,27 @@ const LoginScreen = () => {
                     src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
                     alt=''
                 />
-                <button className='loginScreen__sign-in-button'>Sign In</button>
+                <button onClick={() => setSignIn(true)} className='loginScreen__sign-in-button'>Sign In</button>
                 <div className='loginScreen__gradient' />
             </div>
             <div className='loginScreen__body'>
-                <>
-                    <h1>Unlimited films, TV programmes and more</h1>
-                    <h2>Watch anywhere. Cancel at any time.</h2>
+                {signIn ? (
+                    <SignUpScreen/>
+                ) : (
+                    <>
+                        <h1>Unlimited films, TV programmes and more</h1>
+                        <h2>Watch anywhere. Cancel at any time.</h2>
+                        <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+
+                        <div className='loginScreen__formContainer'>
+                            <form>
+                                <input type='email' placeholder='Email Address'/>
+                                <button onClick={() => setSignIn(true)} className='loginScreen__submitButton'>GET STARTED</button>
+                            </form>
+                        </div>
                     </>
+                )}
+
             </div>
         </div>
     )
