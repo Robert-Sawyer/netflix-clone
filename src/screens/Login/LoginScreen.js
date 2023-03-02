63b16ae908bda8e50b3f279f0ab2cde04beb96b8
Robert-Sawyer <robert.olbrychowski@hotmail.com>
2023-03-02 12:35:21 +0100

    Little changes

--- src/screens/Login/LoginScreen.js
+++ src/screens/Login/LoginScreen.js
@@ -8,11 +8,14 @@ const LoginScreen = () => {
     return (
         <div className='loginScreen'>
             <div className='loginScreen__background'>
-                <img
-                    className='loginScreen__logo'
-                    src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
-                    alt=''
-                />
+                <a href='/'>
+                    <img
+                        className='loginScreen__logo'
+                        src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png'
+                        alt=''
+                    />
+                </a>
+
                 <button onClick={() => setSignIn(true)} className='loginScreen__sign-in-button'>Sign In</button>
                 <div className='loginScreen__gradient' />
             </div>
--- src/screens/SignUp/SignUpScreen.js
+++ src/screens/SignUp/SignUpScreen.js
@@ -3,13 +3,26 @@ import './SignUpScreen.css';
 
 const SignUpScreen = () => {
 
+    const register = (e) => {
+        e.preventDefault();
+    }
+
+    const signIn = (e) => {
+        e.preventDefault();
+    }
+
     return (
         <div className='signupScreen'>
             <form>
                 <h1>SignIn</h1>
                 <input type='email' placeholder='Email'/>
                 <input type='password' placeholder='Password'/>
-                <button>SignIn</button>
+                <button onClick={signIn}>SignIn</button>
+                <h4>
+                    <span className='signupScreen__gray'>New to Netflix? </span>
+                    <span className='signupScreen__link' onClick={register}>Sign Up now.</span>
+
+                </h4>
             </form>
         </div>
     )
